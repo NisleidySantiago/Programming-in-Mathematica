a822abcb9dc992584f92dc68bd9544c9eea0974f
pktpink <36342673+NisleidySantiago@users.noreply.github.com>
2021-08-07 20:41:09 -0400

    Delete Ejercicio3.m

--- Ejercicio3.m
+++
@@ -1,8 +0,0 @@
-(* ::Package:: *)
-
-(* ::Input:: *)
-(*Print["Programa que obtiene todos los numeros enteros entre 2000 y 3000 con raiz cuadrada exacta"]*)
-
-
-(* ::Input:: *)
-(*n=2000;While[n<=3000,If[(Mod[N[Sqrt[n]],2]==0)||(Mod[N[Sqrt[n]],3]==0)||(Mod[N[Sqrt[n]],4]==0)||(Mod[N[Sqrt[n]],5]==0)||(Mod[N[Sqrt[n]],6]==0)||(Mod[N[Sqrt[n]],7]==0)||(Mod[N[Sqrt[n]],8]==0)||(Mod[N[Sqrt[n]],9]==0),Print["Raiz de ",n,"=",Sqrt[n]]];n++]*)
