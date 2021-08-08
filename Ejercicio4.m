e62f0802f9f0eeccf719d90cfcc4c9a162c0de6c
pktpink <36342673+NisleidySantiago@users.noreply.github.com>
2021-08-07 20:41:22 -0400

    Delete Ejercicio4.m

--- Ejercicio4.m
+++
@@ -1,8 +0,0 @@
-(* ::Package:: *)
-
-(* ::Input:: *)
-(*Print["Programa que calcula los numeros mayores que 50 cuyo cuadrado es menor que 3900 "]*)
-(*n=51;While[n*n<3900,Print[n*n," Es cuadrado de ", n];n++]*)
-
-
-
